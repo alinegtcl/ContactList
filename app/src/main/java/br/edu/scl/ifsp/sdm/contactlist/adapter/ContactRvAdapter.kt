e5036e2cbf5cd095944f91751e18f88a594225d4
aline <linegcruz@gmail.com>
2023-11-17 19:14:05 -0300

    Change ListView to RecyclerView.

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/adapter/ContactRvAdapter.kt
@@ -0,0 +1,35 @@
+package br.edu.scl.ifsp.sdm.contactlist.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.scl.ifsp.sdm.contactlist.databinding.TileContactBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
+
+class ContactRvAdapter(private val contactList: MutableList<Contact>) :
+    RecyclerView.Adapter<ContactRvAdapter.ContactViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        TileContactBinding.inflate(LayoutInflater.from(parent.context), parent, false).run {
+            ContactViewHolder(this)
+        }
+
+    override fun getItemCount() = contactList.size
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        contactList[position].also {  contact ->
+            with(holder) {
+                nameTv.text = contact.name
+                emailTv.text = contact.email
+            }
+        }
+    }
+
+    inner class ContactViewHolder(tileContactBinding: TileContactBinding) :
+        RecyclerView.ViewHolder(tileContactBinding.root) {
+        val nameTv: TextView = tileContactBinding.nameTv
+        val emailTv: TextView = tileContactBinding.emailTv
+    }
+
+}
